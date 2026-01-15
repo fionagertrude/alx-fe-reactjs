eb038cc0647ff5e7399d228c894f609fda4fb197
Fiona Gertrude <fionagertrude.k@gmail.com>
2026-01-15 17:19:13 +0300

    Final changes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,18 +1,16 @@
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <div>
       <Header />
+      <WelcomeMessage />
+      <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
       <MainContent />
-      <UserProfile 
-        name="Alice" 
-        age="25" 
-        bio="Loves hiking and photography" 
-      />
       <Footer />
     </div>
   );
