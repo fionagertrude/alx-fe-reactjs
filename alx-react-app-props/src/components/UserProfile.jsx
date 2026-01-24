10c8880b4b5379e10c763185ce1a9d7315d22bb5
Fiona Gertrude <fionagertrude.k@gmail.com>
2026-01-24 10:55:52 +0300

    new commit

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import { useContext } from "react";
+import UserContext from "./UserContext";
 
-function UserProfile(props) {
-    return (
-        <div className="user-profile">
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio}</p>
-        </div>
-    );
+function UserProfile() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
 
 export default UserProfile;
