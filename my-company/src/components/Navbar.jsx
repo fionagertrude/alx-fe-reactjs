3eeac3b62ebc861186203affd5dcec9fe9801a4b
Fiona Gertrude <fionagertrude.k@gmail.com>
2026-01-24 12:57:30 +0300

    new commit

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,10 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+
 function Navbar() {
   return (
-    <nav style={{ position: 'fixed', top: 0, width: '100%', backgroundColor: 'purple', color: 'white', 
-    padding: '10px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', zIndex: 1000 }}>
-      <h2>My Company</h2>
-      <ul style={{ listStyle: 'none', display: 'flex', gap: '20px', margin: 10, }}>
+    <nav style={{ background: '#333', padding: '1rem', color: 'white' }}>
+      <ul style={{ listStyle: 'none', display: 'flex', gap: '1rem', margin: 0, padding: 0 }}>
         <li><Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Home</Link></li>
         <li><Link to="/about" style={{ color: 'white', textDecoration: 'none' }}>About</Link></li>
         <li><Link to="/services" style={{ color: 'white', textDecoration: 'none' }}>Services</Link></li>
